540b88531cf9a3767b30b26c49147382c8dab620
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-08 23:43:24 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,14 +22,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const preload = new createjs.LoadQueue();
     preload.setMaxConnections(5); // Adjust the number of concurrent downloads
 
-    // Reference to the loading bar element
-    const loadingBar = document.getElementById('loadingBar');
-
     // Function to play video by index
     function playVideoByIndex(index) {
-        if (currentVideo) {
+        if (currentVideo && currentVideo.parentNode) {
             currentVideo.pause();
-            videoPlayerContainer.removeChild(currentVideo);
+            currentVideo.parentNode.removeChild(currentVideo);
         }
 
         const newVideo = preloadedVideos[index];
@@ -45,39 +42,37 @@ document.addEventListener('DOMContentLoaded', function () {
         currentVideoIndex = index;
     }
 
-    // Preload all videos with progress tracking
+    // Preload all videos
     preload.loadManifest(videoArray.map(videoPath => ({ src: videoPath })));
 
-    // Add an event listener for progress updates during loading
-    preload.on('progress', function (event) {
-        // Update the width of the loading bar based on progress
-        loadingBar.style.width = (event.progress * 100) + '%';
+    // Add an event listener for when all assets are loaded
+    preload.on('complete', function () {
+        // Create preloaded video elements
+        videoArray.forEach(videoPath => {
+            const video = document.createElement('video');
+            video.src = videoPath;
+            video.preload = 'auto';
+            video.setAttribute('playsinline', ''); // Add playsinline attribute for mobile devices
+            preloadedVideos.push(video);
+        });
 
-        // Check if the loading progress has reached a certain threshold (e.g., 50%)
-        if (event.progress >= 0.5) {
-            // Hide or remove the loading bar element
-            loadingBar.style.display = 'none';
+        // Add a click event listener to switch to the next video on user interaction
+        document.addEventListener('click', () => {
+            // Calculate the next index, wrapping around to the beginning if needed
+            currentVideoIndex = (currentVideoIndex + 1) % videoArray.length;
 
-            // Proceed with the video player logic
+            // Play the next video
+            playVideoByIndex(currentVideoIndex);
 
-            // Add a click event listener to switch to the next video on user interaction
-            document.addEventListener('click', () => {
-                // Calculate the next index, wrapping around to the beginning if needed
-                currentVideoIndex = (currentVideoIndex + 1) % videoArray.length;
+            // Play the background audio using SoundJS only once
+            if (!audioPlaying) {
+                createjs.Sound.registerSound({ src: 'wwwroot/assets/Song.m4a', id: 'backgroundAudio' });
+                const backgroundAudio = createjs.Sound.play('backgroundAudio', { loop: -1 });
+                audioPlaying = true;
+            }
+        });
 
-                // Play the next video
-                playVideoByIndex(currentVideoIndex);
-
-                // Play the background audio using SoundJS only once
-                if (!audioPlaying) {
-                    createjs.Sound.registerSound({ src: 'wwwroot/assets/Song.m4a', id: 'backgroundAudio' });
-                    const backgroundAudio = createjs.Sound.play('backgroundAudio', { loop: -1 });
-                    audioPlaying = true;
-                }
-            });
-
-            // Start with the first video in the array
-            playVideoByIndex(0);
-        }
+        // Start with the first video in the array
+        playVideoByIndex(0);
     });
 });

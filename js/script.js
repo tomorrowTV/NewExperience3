541e60890ca2943b43d22d95bc88d2449e48e9dc
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-09 23:20:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
-    let currentVideo; // Declare currentVideo variable
-    let currentVideoIndex = 0; // Keep track of the current video index
-    let audioPlaying = false; // Flag to track audio playback
-
-    // Create an array to store preloaded video elements
+    let currentVideo;
+    let currentVideoIndex = 0;
+    let audioPlaying = false;
     const preloadedVideos = [];
-
-    // Define the videoArray with the video paths
     const videoArray = [
         'wwwroot/videos/SW1.mp4',
         'wwwroot/videos/SW2.mp4',
@@ -18,14 +14,11 @@ document.addEventListener('DOMContentLoaded', function () {
         // Add more video filenames as needed
     ];
 
-    // Initialize CreateJS PreloadJS
     const preload = new createjs.LoadQueue();
-    preload.setMaxConnections(5); // Adjust the number of concurrent downloads
+    preload.setMaxConnections(5);
 
-    // Reference to the loading bar element
     const loadingBar = document.getElementById('loadingBar');
 
-    // Function to play video by index
     function playVideoByIndex(index) {
         if (currentVideo) {
             currentVideo.pause();
@@ -35,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const newVideo = preloadedVideos[index];
         videoPlayerContainer.appendChild(newVideo);
 
-        newVideo.currentTime = currentVideo ? currentVideo.currentTime : 0; // Sync video time
+        newVideo.currentTime = currentVideo ? currentVideo.currentTime : 0;
 
         currentVideo = newVideo;
         currentVideo.play().catch(error => {
@@ -45,30 +38,30 @@ document.addEventListener('DOMContentLoaded', function () {
         currentVideoIndex = index;
     }
 
-    // Preload all videos with progress tracking
-    preload.loadManifest(videoArray.map(videoPath => ({ src: videoPath })));
+    function preloadVideo(index) {
+        const video = document.createElement('video');
+        video.src = videoArray[index];
+        video.preload = 'auto';
+        video.setAttribute('playsinline', '');
+        preloadedVideos[index] = video;
+    }
 
-    // Add an event listener for progress updates during loading
+    preloadVideo(0); // Only preload the first video initially
+
     preload.on('progress', function (event) {
-        // Update the width of the loading bar based on progress
         loadingBar.style.width = (event.progress * 100) + '%';
 
-        // Check if the loading progress has reached a certain threshold (e.g., 50%)
-        if (event.progress >= 0.2) {
-            // Hide or remove the loading bar element
+        if (event.progress >= 0.5) {
             loadingBar.style.display = 'none';
 
-            // Proceed with the video player logic
-
-            // Add a click event listener to switch to the next video on user interaction
             document.addEventListener('click', () => {
-                // Calculate the next index, wrapping around to the beginning if needed
                 currentVideoIndex = (currentVideoIndex + 1) % videoArray.length;
 
-                // Play the next video
+                // Preload the next video on-demand
+                preloadVideo(currentVideoIndex);
+
                 playVideoByIndex(currentVideoIndex);
 
-                // Play the background audio using SoundJS only once
                 if (!audioPlaying) {
                     createjs.Sound.registerSound({ src: 'wwwroot/assets/Song.m4a', id: 'backgroundAudio' });
                     const backgroundAudio = createjs.Sound.play('backgroundAudio', { loop: -1 });
@@ -76,7 +69,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
 
-            // Start with the first video in the array
             playVideoByIndex(0);
         }
     });

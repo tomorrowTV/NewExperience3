e12c1e9eed977d38ca7297da72c7f376f8e3f610
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-11-10 21:50:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
     let currentVideoIndex = 0;
     let audioPlaying = false;
+    let audioStartTime = 0;
     const preloadedVideos = [];
+    let assetsLoaded = 0;
 
     // Define assets to preload
     const assetsToLoad = [
@@ -26,12 +28,28 @@ document.addEventListener('DOMContentLoaded', function () {
     function playVideoByIndex(index) {
         const newVideo = preloadedVideos[index];
         videoPlayerContainer.innerHTML = ''; // Clear container
+
+        // Add the 'playsinline' attribute for mobile devices
+        newVideo.setAttribute('playsinline', '');
+
         videoPlayerContainer.appendChild(newVideo);
 
-        newVideo.currentTime = 0; // Reset video time
-        newVideo.play().catch(error => {
-            console.error('Video playback error:', error.message);
-        });
+        // Check if the video is fully loaded before attempting to play
+        if (newVideo.readyState >= 3) {
+            newVideo.currentTime = audioStartTime; // Set the current time in the video to match the audio start time
+            newVideo.play().catch(error => {
+                console.error('Video playback error:', error.message);
+            });
+        } else {
+            // If not fully loaded, wait for the 'canplay' event to play the video
+            newVideo.addEventListener('canplay', function onCanPlay() {
+                newVideo.removeEventListener('canplay', onCanPlay);
+                newVideo.currentTime = audioStartTime; // Set the current time in the video to match the audio start time
+                newVideo.play().catch(error => {
+                    console.error('Video playback error:', error.message);
+                });
+            });
+        }
     }
 
     // Preload assets with progress tracking
@@ -54,32 +72,47 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
 
-            if (preloadedVideos.length > 0) {
+            if (preloadedVideos.some(video => !!video)) { // Check if at least one video is preloaded
                 // Start the game when at least one video is preloaded
                 startGame();
             }
         }
     });
 
+    // Add an event listener for when each asset is loaded
+    preload.on('fileload', function (event) {
+        assetsLoaded++;
+        console.log('Assets loaded:', assetsLoaded);
+
+        // Check if the loaded asset is a video and it's not yet in the preloadedVideos array
+        if (event.item.src.endsWith('.mp4') && !preloadedVideos.some(video => video.src === event.result.src)) {
+            preloadedVideos.push(event.result);
+        }
+
+        // Trigger the start of the game when at least one video is preloaded
+        if (preloadedVideos.length >= 1) {
+            startGame();
+        }
+    });
+
     // Add an event listener for when all assets are loaded
     preload.on('complete', function () {
         loadingBar.style.display = 'none'; // Hide loading bar
+        console.log('All assets loaded');
+        if (preloadedVideos.length === 0) {
+            console.warn('No videos preloaded');
+        }
     });
 
     function startGame() {
         // Add a click event listener to switch to the next video on user interaction
         document.addEventListener('click', function () {
+            // Set the audio start time to match the current time in the current video
+            audioStartTime = preloadedVideos[currentVideoIndex].currentTime;
+
             currentVideoIndex = (currentVideoIndex + 1) % preloadedVideos.length;
             playVideoByIndex(currentVideoIndex);
 
             if (!audioPlaying) {
                 createjs.Sound.registerSound({ src: 'wwwroot/assets/Song.m4a', id: 'backgroundAudio' });
-                const backgroundAudio = createjs.Sound.play('backgroundAudio', { loop: -1 });
-                audioPlaying = true;
-            }
-        });
-
-        // Start with the first video in the array
-        playVideoByIndex(0);
-    }
-});
+                const backgroundAudio = createjs.Sound.play('backgroundAudio
